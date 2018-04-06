92b7644d98a25de0bf85fe8b8f7535b18c47fbb2
Mark <mark.lukin36@gmail.com>
2018-04-06 15:47:16 +0300

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323130824) do
+ActiveRecord::Schema.define(version: 20180323134550) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180323130824) do
     t.decimal "balance", default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
