1976491d80b9af4ea304786b77a2aca9802df2ea
Mark <mark.lukin36@gmail.com>
2018-04-13 16:54:59 +0300

    Generate devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_secure_password
   VALID_EMAIL_REGEX = /[\wа-я\.\+]+@([\wа-я]+\.?)+/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
   before_save { self.email = email.downcase }
