b8b7c2c078651ff843c992ff5a0ffe7162d46bd2
Mark <mark.lukin36@gmail.com>
2018-04-13 16:18:43 +0300

    Password methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
   VALID_EMAIL_REGEX = /[\wа-я\.\+]+@([\wа-я]+\.?)+/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password_digest, presence: true
-
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   before_save { self.email = email.downcase }
 end
